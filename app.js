a3f66eeffc930ba6063b30d5d3bde3ef5a17c0c0
Rajpreetsingh12 <141981724+Rajpreetsingh12@users.noreply.github.com>
2024-10-26 18:38:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 app.get('/', (req, res) => {
-res.send('Hello, World! This is a GCP demo app.');
+res.send('Hello, World! This is  gcp demo app by rajee.');
 });
 app.listen(PORT, () => {
 console.log(`Server is running on port ${PORT}`);
